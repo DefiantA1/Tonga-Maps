232717c0ad3618b16d8d7a282ba3048ae1e9445b
Arthur Cocker <arthurcocker02@gmail.com>
2026-06-09 05:52:35 +1300

    batman

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+export const containerStyle = {
+    width: '100%',
+    height: '80vh',
+    borderRadius: '15px 0px 0px 15px',
+};
+
+const zoom = 18
+
+const center = {
+  lat: -21.1394,
+  lng: -175.2049, // Nuku'alofa
+};
+
+export default function Home() {
+  
+  return (
+    <></>
+  );
+}
+
