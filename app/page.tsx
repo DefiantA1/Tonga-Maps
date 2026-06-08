508a293593117634445dda5f833431b1d8b306da
Arthur Cocker <arthurcocker02@gmail.com>
2026-06-09 09:12:19 +1300

    add marker to where you click and pan to that location

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { AdvancedMarker, APIProvider, Map} from "@vis.gl/react-google-maps";
-import { useEffect, useState } from "react";
+import { AdvancedMarker, APIProvider, Map, MapMouseEvent, useMap} from "@vis.gl/react-google-maps";
+import { useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 
 export const containerStyle = {
@@ -12,27 +12,69 @@ export const containerStyle = {
 
 const zoom = 15
 
-const center = {
+const nukualofa = {
   lat: -21.1394,
   lng: -175.2049, // Nuku'alofa
 };
 
 export default function Home() {
+  const dragged = useRef(false);
+  const [markerPosition, setMarkerPosition] = useState<{lat: number, lng: number} | null>(null);
+
+  function handleMapClick(e: MapMouseEvent){
+    // returns if a drag was detected
+    if(dragged.current){
+      dragged.current = false;
+      return;
+    }
+
+    const latLng = e.detail.latLng;
+
+    setMarkerPosition({
+      lat: latLng.lat,
+      lng: latLng.lng,
+    });
+  }
+
   return (
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
         <Map
           style={{width: '100vw', height: '100vh'}}
           mapId={process.env.NEXT_PUBLIC_MAP_ID!}
-          defaultCenter={center}
+          defaultCenter={nukualofa}
           defaultZoom={zoom}
           gestureHandling='greedy'
           colorScheme="DARK"
+          onDragstart={() => {
+            dragged.current = true;
+          }}
+          onClick={(e) => handleMapClick(e)}
           disableDefaultUI
         >
           <MyLocationMarker/>
+          <PanController target={markerPosition} />
+          {
+            markerPosition != null && <AdvancedMarker position={markerPosition} />
+          }
         </Map>
       </APIProvider>
   );
+}
+
+function PanController({
+  target,
+}: {
+  target: any | null;
+}) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (map && target) {
+      map.panTo(target);
+    }
+  }, [map, target]);
+
+  return null;
 }
 
 function MyLocationMarker(){
@@ -40,8 +82,7 @@ function MyLocationMarker(){
   const [location, setLocation] = useState<{lat: number, lng: number} | null>(null);
   
   useEffect(() => {
-
-    const getLocation = () => {
+    const getMyLocation = () => {
       if(!navigator.geolocation){
         console.log('Geolocation is not supported');
         return;
@@ -55,33 +96,36 @@ function MyLocationMarker(){
       }, (err) => {}, {enableHighAccuracy: true, timeout: 15000});
     }
 
-    getLocation();
-
+    getMyLocation();
   }, [])
   
   return (
-    <AdvancedMarker
-      position={center}
-      zIndex={2}
-      title={'myLocation'}
-      onClick={() => toast.info("That's My Location")}
-    >
-      <div className="relative flex items-center justify-center">
-          <span
-            className="absolute size-8 animate-ping rounded-full opacity-40"
-            style={{ backgroundColor: "#3eb06b" }}
-          />
-          <span
-            className={`relative block size-3.5 rounded-full border-2 ${
-                "scale-125 ring-2 ring-white/30" 
-            }`}
-            style={{
-              backgroundColor: "#3eb06b",
-              borderColor: `#ffffff99`,
-              boxShadow: `0 0 12px #ffffff66`,
-            }}
-          />
-        </div>
-    </AdvancedMarker>
+    <>
+      {
+        location != null && <AdvancedMarker
+          position={location}
+          zIndex={2}
+          title={'myLocation'}
+          onClick={() => toast.info("That's My Location")}
+        >
+          <div className="relative flex items-center justify-center">
+              <span
+                className="absolute size-8 animate-ping rounded-full opacity-40"
+                style={{ backgroundColor: "#3eb06b" }}
+              />
+              <span
+                className={`relative block size-3.5 rounded-full border-2 ${
+                    "scale-125 ring-2 ring-white/30" 
+                }`}
+                style={{
+                  backgroundColor: "#3eb06b",
+                  borderColor: `#ffffff99`,
+                  boxShadow: `0 0 12px #ffffff66`,
+                }}
+              />
+            </div>
+        </AdvancedMarker>
+      }
+    </>
   );
 }
