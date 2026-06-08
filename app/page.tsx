7a2cf937f33de31773c416aa62cfd7026c4a923c
Arthur Cocker <arthurcocker02@gmail.com>
2026-06-09 06:00:39 +1300

    add google maps

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
+import { APIProvider, Map} from "@vis.gl/react-google-maps";
+
 export const containerStyle = {
     width: '100%',
     height: '80vh',
     borderRadius: '15px 0px 0px 15px',
 };
 
-const zoom = 18
+const zoom = 15
 
 const center = {
   lat: -21.1394,
@@ -14,9 +16,19 @@ const center = {
 };
 
 export default function Home() {
-  
   return (
-    <></>
+      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
+        <Map
+          style={{width: '100vw', height: '100vh'}}
+          mapId={process.env.NEXT_PUBLIC_MAP_ID!}
+          defaultCenter={center}
+          defaultZoom={zoom}
+          gestureHandling='greedy'
+          colorScheme="DARK"
+          disableDefaultUI
+        >
+        </Map>
+      </APIProvider>
   );
 }
 
