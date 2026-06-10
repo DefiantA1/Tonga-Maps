02c7919b5442d0dee78eb737a3ae0172dd9800dd
Arthur Cocker <arthurcocker02@gmail.com>
2026-06-11 12:22:16 +1300

    Add Login Page

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage(){
+    return (
+        <div style={{backgroundColor: '#1c2a40'}} className="w-screen h-screen">
+            <p>This is the login Page</p>
+        </div>
+    );
+}
