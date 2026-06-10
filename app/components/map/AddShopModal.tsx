e3bff82a37ed9472b23814a30e7fd8e8d1c41e41
Arthur Cocker <arthurcocker02@gmail.com>
2026-06-10 19:38:41 +1300

    add shop data to firebase

--- app/components/map/AddShopModal.tsx
+++ app/components/map/AddShopModal.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { db, storage } from "@/app/firebase/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { ShoppingCart, X } from "lucide-react";
 import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 import { toast } from "react-toastify";
@@ -7,20 +10,27 @@ import { toast } from "react-toastify";
 type AddShopModalProps = {
   isOpen: boolean,
   exit: () => void,
+  markerPosition: {lat: number, lng: number} | null
 }
 
-export function AddShopModal({isOpen, exit} : AddShopModalProps){
+export function AddShopModal({isOpen, exit, markerPosition} : AddShopModalProps){
   const [name, setName] = useState<string>("");
   const [acceptsBSP, setAcceptsBSP] = useState<boolean>(false);
   const [acceptsANZ, setAcceptsANZ] = useState<boolean>(false);
 
+  const [imgFile, setImgFile] = useState<File | null>();
+
   const [loading, setLoading] = useState<boolean>(false);
+
+  const [comment, setComment] = useState<string>("");
   
   function exitModal(){
     setName("");
+    setComment("");
     setAcceptsANZ(false);
     setAcceptsBSP(false);
     setLoading(false);
+    setImgFile(null);
 
     exit();
   }
@@ -43,17 +53,20 @@ export function AddShopModal({isOpen, exit} : AddShopModalProps){
             <h2 className="mr-3" style={{fontWeight: 'bold'}}>Add a Shop</h2>
             <ShoppingCart/>
           </div>
-          <X onClick={() => exitModal()}/>
+          <X className="cursor-pointer" onClick={() => exitModal()}/>
         </div>
         <hr className="mt-2 mb-4"/>
         <Field title={"Shop Name"} value={name} setValue={setName}/>
-        <ImgField/>
+        <ImgField onFileChange={(f: File | null) => setImgFile(f)}/>
         <CardBoxes
           anz={acceptsANZ}
           bsp={acceptsBSP}
           onANZChange={(v) => setAcceptsANZ(v)} 
           onBSPChange={(v) => setAcceptsBSP(v)}
         />
+        <div className="mt-3">
+          <Field title={'Comment (Optional)'} setValue={setComment} value={comment}/>
+        </div>
         <AddBtn/>
       </div>
     </div>
@@ -75,7 +88,6 @@ export function AddShopModal({isOpen, exit} : AddShopModalProps){
   async function handleAddShop(){
     try{
       if(loading){
-        toast.info('Still loading...');
         return;
       }
 
@@ -87,17 +99,47 @@ export function AddShopModal({isOpen, exit} : AddShopModalProps){
         throw new Error('Which card does this shop provide?');
       }
 
-      setLoading(true);
+      if(markerPosition == null){
+        throw new Error('Shop location not found');
+      }
 
       const newShop : Shop = {
         name: name,
         acceptsANZ: acceptsANZ,
         acceptsBSP: acceptsBSP,
         createdAt: new Date().getTime(),
+        lat: markerPosition.lat,
+        lng: markerPosition.lng,
+        pending: true,
       };
+
+      setLoading(true);
+
 
       const toastId = toast.loading('Uploading Now...');
 
+      let imgUrl : null | string = null;
+      
+      if(imgFile != null){
+        const fileName = `${name.replaceAll(" ", "")}-${new Date().getTime()}`;
+        const storageRef = ref(storage, `markers/${fileName}`)
+
+        // show "updating img" in button
+        await uploadBytes(storageRef, imgFile);
+        // show "completed"
+
+        imgUrl = await getDownloadURL(storageRef);
+        newShop.imgUrl = imgUrl;
+      }
+      
+      if(comment != ''){
+        newShop.comment = comment;
+      }
+
+      const dbPath = collection(db, 'markers');
+      await addDoc(dbPath,newShop)
+
+      exitModal();
       
       toast.update(toastId, {
         type: 'success',
@@ -137,13 +179,17 @@ type FieldProps = {
 function Field({title, setValue, value} : FieldProps){
   return (
     <div className="mb-4">
-      <p>{title}</p>
+      <p className="text-gray-600">{title}</p>
       <input value={value} onChange={(e) => setValue(e.target.value)} className="bg-gray-200 w-full p-2 rounded"/>
     </div>
   );
 }
 
-function ImgField(){
+type ImgFieldProps = {
+  onFileChange: (f: File | null) => void
+}
+
+function ImgField({onFileChange} : ImgFieldProps){
   const [fileName, setFileName] = useState<string>('');
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
@@ -154,17 +200,19 @@ function ImgField(){
       
       const objectUrl = URL.createObjectURL(files[0]);
       setPreviewUrl(objectUrl);
+      onFileChange(files[0]);
     } else {
       setFileName('');
+      onFileChange(null);
     }
   };
 
   return (
-    <div className="mb-4">
+    <div className="mb-4 text-gray-600">
       <p className="mb-2">Picture of Shop (Optional)</p>
       
       {
-        previewUrl == null && <label htmlFor="image-picker-input" className="cursor-pointer rounded-md p-3 text-sm text-black text-center bg-gray-200 w-full block hover:bg-blue-500 transition-colors">Upload Photo (Optional)</label>
+        previewUrl == null && <label htmlFor="image-picker-input" className="cursor-pointer rounded-md p-3 text-sm text-gray-800 text-center bg-gray-200 w-full block hover:bg-blue-500 transition-colors">Upload Photo (Optional)</label>
       }
 
       <input id="image-picker-input" className="hidden" type="file" accept="image/*" onChange={(e) => handleFileChange(e)}/>
@@ -194,15 +242,15 @@ type CardBoxesProps = {
 
 function CardBoxes({onBSPChange, onANZChange, anz, bsp} : CardBoxesProps){
   return (
-    <div className="mt-5">
+    <div className="mt-5 text-gray-600">
       <p>This Shop Accepts</p>
       <div className="flex flex-row items-center">
         <input checked={bsp} onChange={(e) => onBSPChange(e.target.checked)} id="bspCheckBox" type="checkbox" className="bg-gray-300 checked:bg-indigo-600 m-2 w-5 h-5 rounded"/>
-        <label htmlFor="bspCheckBox">BSP</label>
+        <label htmlFor="bspCheckBox" className="text-gray-800">BSP</label>
       </div>
       <div className="flex flex-row items-center">
         <input checked={anz} onChange={(e) => onANZChange(e.target.checked)} id="anzCheckBox" type="checkbox" className="bg-gray-300 m-2 w-5 h-5 rounded"/>
-        <label htmlFor="anzCheckBox">ANZ</label>
+        <label htmlFor="anzCheckBox" className="text-gray-800">ANZ</label>
       </div>
     </div>
   );
