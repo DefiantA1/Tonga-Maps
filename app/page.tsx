7f1ff9b4133312228f88b3dde353c5140f22e3db
Arthur Cocker <arthurcocker02@gmail.com>
2026-06-12 12:17:41 +1300

    format sidebar UI

--- app/page.tsx
+++ app/page.tsx
@@ -76,67 +76,74 @@ export default function Home() {
   }, [])
 
 
-  return (
-    <div className="flex flex-row">
-      <div style={{backgroundColor: '#171414'}} className="w-80 h-screen hidden lg:block p-5">
-        <h2 className="text-xl font-semibold">Tonga Maps</h2>
-        <p>Shops That Accept Card Payments</p>
-        <ul>
-          {shops.length != 0 && shops.sort((a,b) => a.name.localeCompare(b.name)).map((s) => <li key={s.id} onClick={() => {handleShopTileClick({lat: s.lat, lng: s.lng})}}>
-            <div className="flex flex-row gap-3 my-3">
-              <ShoppingCart/>
-              <p>{s.name}</p>
+  return (      
+    <div className="bg-blue-300 h-screen">
+      <div className="relative w-full h-full">
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
+          <Map
+            style={{width: '100%', height: '100%'}}
+            mapId={process.env.NEXT_PUBLIC_MAP_ID!}
+            defaultCenter={nukualofa}
+            defaultZoom={zoom}
+            gestureHandling='greedy'
+            colorScheme="DARK"
+            onDragstart={() => {
+              dragged.current = true;
+            }}
+            onClick={(e) => handleMapClick(e)}
+            keyboardShortcuts={false}
+            disableDefaultUI={true}
+            mapTypeId={mapTypeId}
+          >
+              <MyMarker/>
+              <PanController target={markerPosition} />
+              {
+                markerPosition != null && <AdvancedMarker position={markerPosition} />
+              }
+              <AddShopModal 
+                  isOpen={shopModalOpen} 
+                  exit={() => {
+                    setShopModalOpen(false);
+                    setMarkerPosition(null);
+                  }}
+                  markerPosition={markerPosition}
+              />
+              <LoginModal isOpen={loginModalOpen} exit={() => setLoginModalOpen(false)}/>
+              {
+                shops.filter((s) => !s.pending || s.uid == localStorage.getItem('uid') || localStorage.getItem('type') == 'super').map((s) => (<ShopMarker key={s.id} shop={s}/>))
+              }
+          </Map>
+        </APIProvider>
+        <div className="absolute top-5 right-2 flex flex-row gap-3 justify-center items-center">
+          <Switch value={mapTypeId == 'roadmap'} onChange={() => toggleMap()}/>
+          <p className="bg-gray-600 px-2 py-1 rounded" onClick={() => toggleMap()}>{mapTypeId == 'roadmap' ? 'Road Map' : 'Satellite'}</p>
+          {
+            <div onClick={() => goToSettingPage()} className="bg-gray-600 p-1 rounded-full">
+              <Settings/>
             </div>
-          </li>)}
-        </ul>
-      </div>
-      <div className="bg-blue-300 h-screen flex-1">
-        <div className="relative w-full h-full">
-          <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
-            <Map
-              style={{width: '100%', height: '100%'}}
-              mapId={process.env.NEXT_PUBLIC_MAP_ID!}
-              defaultCenter={nukualofa}
-              defaultZoom={zoom}
-              gestureHandling='greedy'
-              colorScheme="DARK"
-              onDragstart={() => {
-                dragged.current = true;
-              }}
-              onClick={(e) => handleMapClick(e)}
-              keyboardShortcuts={false}
-              disableDefaultUI={true}
-              mapTypeId={mapTypeId}
-            >
-                <MyMarker/>
-                <PanController target={markerPosition} />
-                {
-                  markerPosition != null && <AdvancedMarker position={markerPosition} />
-                }
-                <AddShopModal 
-                    isOpen={shopModalOpen} 
-                    exit={() => {
-                      setShopModalOpen(false);
-                      setMarkerPosition(null);
-                    }}
-                    markerPosition={markerPosition}
-                />
-                <LoginModal isOpen={loginModalOpen} exit={() => setLoginModalOpen(false)}/>
-                {
-                  shops.filter((s) => !s.pending || s.uid == localStorage.getItem('uid') || localStorage.getItem('type') == 'super').map((s) => (<ShopMarker key={s.id} shop={s}/>))
-                }
-            </Map>
-          </APIProvider>
-          <div className="absolute top-5 right-2 flex flex-row gap-3 justify-center items-center">
-            <Switch value={mapTypeId == 'roadmap'} onChange={() => toggleMap()}/>
-            <p className="bg-gray-600 px-2 py-1 rounded" onClick={() => toggleMap()}>{mapTypeId == 'roadmap' ? 'Road Map' : 'Satellite'}</p>
-            {
-              <div onClick={() => goToSettingPage()} className="bg-gray-600 p-1 rounded-full">
-                <Settings/>
-              </div>
-            }
-          </div>
-          <div className="absolute top-4 left-4">
+          }
+        </div>
+        <div className="absolute top-4 left-4">
+          <div className="flex flex-row gap-4">
+            <div style={{backgroundColor: '#F8FAFD'}} className="w-80 border border-white opacity-90 h-185 p-5 hidden lg:block rounded-xl overflow-y-auto">
+              <h2 style={{color: '#0F172A'}} className="text-xl font-semibold">Tonga Maps</h2>
+              <p className="text-sm" style={{color: '#64748B'}}>Shops That Accept Card Payments</p>
+              <hr className="border border-gray-400 my-3"/>
+              <ul>
+                {shops.length != 0 && shops.sort((a,b) => a.name.localeCompare(b.name)).map((s) => 
+                  <li className="cursor-pointer" key={s.id} onClick={() => {handleShopTileClick({lat: s.lat, lng: s.lng})}}>
+                    <div className="flex flex-row gap-4 my-3 items-center">
+                      <div className="relative h-8 w-8 flex flex-row">
+                        <div className="absolute rounded p-1 opacity-5 h-9 w-9" style={{backgroundColor: '#0F172A'}}>
+                        </div>
+                        <ShoppingCart style={{color: '#64748B'}} className="cursor-pointer absolute text-black top-2 left-2" size={"20"}/>
+                      </div>
+                      <p className="cursor-pointer" style={{color: '#334155'}}>{s.name}</p>
+                    </div>
+                  </li>
+                )}
+              </ul>
+            </div>
             <img src={'/defiant-logo.png'} className="h-12 w-12"/>
           </div>
         </div>
