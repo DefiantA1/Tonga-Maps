090293af7d36318ac39aa3926bb2129a1a2e7c09
Arthur Cocker <arthurcocker02@gmail.com>
2026-06-12 13:33:02 +1300

    track details

--- app/api/track-visit/route.ts
+++ app/api/track-visit/route.ts
@@ -5,14 +5,20 @@ import { headers } from "next/headers";
 export async function POST(req: Request) {
     const ip = await getIpAddress();
     const deviceType = await getDeviceType();
-
     const ms : number = new Date().getTime();
+
+    const response = await fetch(
+        `http://ip-api.com/json/${ip}`
+    );
+
+    const ipData = await response.json();
     
     const docRef = collection(db, 'visits');
     await addDoc(docRef, {
         ip: ip,
         deviceType: deviceType,
-        dt: Timestamp.fromMillis(ms)
+        dt: Timestamp.fromMillis(ms),
+        ...(ipData),
     })
     
     return new Response('Success', {
